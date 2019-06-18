e6545daf6f7d99321aad0477635f8414e498c513
demo <demo@demos-MacBook-Pro-3.local>
2019-06-18 23:06:27 +0800

    updates

--- charity_models.py
+++ charity_models.py
@@ -1,3 +1,14 @@
-#from google.appengine.ext import ndb
+from google.appengine.ext import ndb
 
-#class Charities(ndb.Model):
+class Charities(ndb.Model):
+    charity_name = ndb.StringProperty(required=True)
+    cause = ndd.StringProperty(required = True)
+
+class Username(ndb.Model):
+    username_id = ndb.IntegerProperty(required = True)
+    first_name = ndb.StringProperty(required = True)
+    last_name = ndb.StringProperty(required = True)
+
+class Donation(ndb.Model):
+    amount = ndb.IntegerProperty(required=True)
+    
