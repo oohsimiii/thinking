66783257376b45363cea89b68e0394d09ab9b5a2
spookyjos <demo@spookyjos.local>
2019-06-19 09:33:11 +0800

    xd

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ jinja_env = jinja2.Environment(
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
         login_url = users.create_login_url("/")
+        dd = {"Login": login_url}
         start_template = jinja_env.get_template("templates/LoginHandler.html")
-        self.response.write(start_template.render())
-        self.response.write('<a href= "' +
-         login_url +'">Login with Gmail.</a>')
+        self.response.write(start_template.render(dd))
+
         user = users.get_current_user()
         if user:
             self.redirect("/loggedin")
@@ -49,8 +49,11 @@ class charityAmountHandler(webapp2.RequestHandler):
         if not user:
             self.redirect("/nouser")
         else:
+            email = user.nickname()
+            logout_url = users.create_logout_url("/")
+            dd = {"Loginout": logout_url, "Loginoutresponse": "Logout", "username": email}
             Amount_template = jinja_env.get_template("templates/charityAmountHandler.html")
-            self.response.write(Amount_template.render())
+            self.response.write(Amount_template.render(dd))
 
 
 class LeaderboardHandler(webapp2.RequestHandler):
@@ -67,11 +70,30 @@ class PersonalHandler(webapp2.RequestHandler):
         else:
             self.redirect("/nouser")
 
+class aboutHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            email = user.nickname()
+            logout_url = users.create_logout_url("/")
+            dd = {"Loginout": logout_url, "Loginoutresponse": "Logout", "username": email}
+            about_template = jinja_env.get_template("templates/aboutHandler.html")
+            self.response.write(about_template.render(dd))
+        else:
+            email = "Main Page"
+            login_url = users.create_login_url("/")
+            dd = {"Loginout": login_url, "Loginoutresponse": "Login", "username": email}
+            about_template = jinja_env.get_template("templates/aboutHandler.html")
+            self.response.write(about_template.render(dd))
+
+
+
 app = webapp2.WSGIApplication([
     ('/', LoginHandler),
     ('/loggedin', MainHandler),
     ('/nouser', nouserHandler),
     ('/charityamount', charityAmountHandler),
     ('/Leaderboard', LeaderboardHandler),
-    ('/personal', PersonalHandler)
+    ('/personal', PersonalHandler),
+    ('/about', aboutHandler)
 ], debug=True)
