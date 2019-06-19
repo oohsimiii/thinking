de3438979c873e009ce9b9777ce3e9afc5a03d46
spookyjos <demo@spookyjos.local>
2019-06-19 17:06:24 +0800

    eS Df bsbbergbs

--- charity_models.py
+++ charity_models.py
@@ -4,10 +4,10 @@ class Charity(ndb.Model):
     charity_name = ndb.StringProperty(required=True)
     link = ndb.StringProperty(required = True)
 
-class Username(ndb.Model):
-    username_id = ndb.IntegerProperty(required = True)
-    first_name = ndb.StringProperty(required = True)
-    last_name = ndb.StringProperty(required = True)
+#class Username(ndb.Model):
+    #username_id = ndb.IntegerProperty(required = True)
+    #first_name = ndb.StringProperty(required = True)
+    #last_name = ndb.StringProperty(required = True)
 
-class Donation(ndb.Model):
-    amount = ndb.IntegerProperty(required=True)
+#class Donation(ndb.Model):
+    #amount = ndb.IntegerProperty(required=True)
