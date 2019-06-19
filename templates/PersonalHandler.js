0025006b87a0c4600560582408fe2a53086ec2f2
demo <demo@demos-MacBook-Pro-6.local>
2019-06-19 16:26:27 +0800

    changes

--- templates/PersonalHandler.js
+++ templates/PersonalHandler.js
@@ -1,5 +1,10 @@
-$("#profileImage").click(function(e) {
-    $("#imageUpload").click();
-});
+
+function(ReplaceImage) {
+    $("#profileImage").replace(".profile_photo");
+
+$("#profileImage").click(function(ReplaceImage)
+);
+
+
 
 $(document).ready(init);
